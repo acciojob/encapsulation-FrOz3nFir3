e89b0ae781947fefae2e333d93a31e94cb054505
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-12-27 12:54:42 +0530

    instantiating an object and using its methods

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly person = new RWOnly();
+    // person.name = "Kush";
+    // Error 'name' has private access in 'com.driver.RWOnly'
+
+    person.setName("Kush");
+    System.out.println(person.getName());
+  }
 }
