7199349b76054a8350aa5d208273c8d325bbcf3e
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-12-27 12:37:10 +0530

    added getter and setters

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String getName() {
+        // getter function
+        return name;
+    }
+
+    public void setName(String name) {
+        // setter function
+        this.name = name;
+    }
 }
